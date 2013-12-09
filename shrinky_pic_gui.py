70902121d550fca15aadd3319669a9dba4abc43d
Dennis Drescher <dennis_drescher@sil.org>
2013-12-09 16:35:42 +0700

    Added icon to app

--- shrinky_pic_gui.py
+++ shrinky_pic_gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'shrinky_pic_gui.ui'
 #
-# Created: Sun Dec  8 21:15:06 2013
+# Created: Mon Dec  9 16:32:36 2013
 #      by: pyside-uic 0.2.13 running on PySide 1.1.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,6 +13,9 @@ class Ui_Form(object):
 	def setupUi(self, Form):
 		Form.setObjectName("Form")
 		Form.resize(322, 385)
+		icon = QtGui.QIcon()
+		icon.addPixmap(QtGui.QPixmap("icons/shrinkypic_32.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+		Form.setWindowIcon(icon)
 		self.horizontalLayout = QtGui.QHBoxLayout(Form)
 		self.horizontalLayout.setObjectName("horizontalLayout")
 		self.gridLayout = QtGui.QGridLayout()
@@ -78,15 +81,15 @@ class Ui_Form(object):
 		self.horizontalLayout.addLayout(self.gridLayout)
 
 		self.retranslateUi(Form)
-#        QtCore.QObject.connect(self.OkButton, QtCore.SIGNAL("pressed()"), Form.setupUi)
-#        QtCore.QObject.connect(self.CaptionEdit, QtCore.SIGNAL("textChanged(QString)"), Form.setupUi)
-#        QtCore.QObject.connect(self.GetPictureButton, QtCore.SIGNAL("clicked()"), Form.setupUi)
+		QtCore.QObject.connect(self.OkButton, QtCore.SIGNAL("pressed()"), Form.setupUi)
+		QtCore.QObject.connect(self.CaptionEdit, QtCore.SIGNAL("textChanged(QString)"), Form.setupUi)
+		QtCore.QObject.connect(self.GetPictureButton, QtCore.SIGNAL("clicked()"), Form.setupUi)
 		QtCore.QObject.connect(self.CancelButton, QtCore.SIGNAL("clicked()"), Form.close)
 		QtCore.QObject.connect(self.RotationDial, QtCore.SIGNAL("sliderMoved(int)"), self.RotationBox.setValue)
 		QtCore.QMetaObject.connectSlotsByName(Form)
 
 	def retranslateUi(self, Form):
-		Form.setWindowTitle(QtGui.QApplication.translate("Form", "ShrinkyPic", None, QtGui.QApplication.UnicodeUTF8))
+		Form.setWindowTitle(QtGui.QApplication.translate("Form", "ShrinkyPic v0.1", None, QtGui.QApplication.UnicodeUTF8))
 		self.PictureRotationLabel.setText(QtGui.QApplication.translate("Form", "Picture Rotation", None, QtGui.QApplication.UnicodeUTF8))
 		self.FileNameLabel.setText(QtGui.QApplication.translate("Form", "File Name", None, QtGui.QApplication.UnicodeUTF8))
 		self.CaptionEdit.setToolTip(QtGui.QApplication.translate("Form", "<html><head/><body><p>Enter a caption for this picture.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
